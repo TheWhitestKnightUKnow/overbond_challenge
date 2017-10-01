0bc76c3869433fe2e2273a0a79a6335329d88593
Rio Kivell <rjckivel@gmail.com>
2017-09-30 22:01:48 -0400

    Added 2 sample files, and skeleton solution files

---
+++ curve.py
@@ -0,0 +1,7 @@
+import sys
+import csv
+
+def main():
+    if len(sys.argv) < 2:
+        print "Please pass me a csv file to parse!"
+        return
---
+++ yield.py
@@ -0,0 +1,9 @@
+import sys
+import csv
+
+def main():
+    if len(sys.argv) < 2:
+        print "Please pass me a csv file to parse!"
+        return
+    
+
