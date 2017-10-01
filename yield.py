6b0653267092a9ac526fbb35fea12955aec605de
Rio Kivell <rjckivel@gmail.com>
2017-09-30 23:50:28 -0400

    Updated csv parsing, included exception handling.

--- yield.py
+++ yield.py
@@ -2,13 +2,41 @@ import sys
 import csv
 
 def main():
+    # If there wasn't a CSV passed, or user is looking for help,
+    # let them know what to do!
     if len(sys.argv) < 2 or sys.argv[1] == '-h' or sys.argv[1] == '--help':
         print "Please pass me a csv file to parse!"
         return
-    with open(sys.argv[1], 'rb') as bondinfo:
+    try:
+        # Otherwise, open up the file...
+        bondinfo = open(sys.argv[1], 'rb')
+    except IOError as e:
+        print "Yield.py failed to open this file: " + sys.argv[1]
+        print "Please pass it a valid csv file containing bond information!"
+        return
+    else:
+        # ...and parse its rows.
+        corporate_bonds  = []
+        government_bonds = []
         reader = csv.reader(bondinfo, delimiter=',')
         for row in reader:
-            print row
+            if row[0] == 'bond': # first row
+                continue
+            elif row[1] == 'corporate':
+                corporate_bonds.append({
+                    "name": row[0],
+                    "term": float(row[2].split(' ')[0]),
+                    "yield": float(row[3][:-1])
+                })
+            elif row[1] == 'government':
+                government_bonds.append({
+                    "name": row[0],
+                    "term": float(row[2].split(' ')[0]),
+                    "yield": float(row[3][:-1])
+                })
+            else:
+                print 'This CSV contained a bond that wasn\'t government or corporate!'
+                return
 
 main()
 
