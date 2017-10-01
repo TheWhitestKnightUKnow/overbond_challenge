b82fb35f7bc416718c7780c499ae8dd3266d0beb
Rio Kivell <rjckivel@gmail.com>
2017-09-30 23:05:11 -0400

    Got basic command line csv parsing up and running

--- yield.py
+++ yield.py
@@ -2,8 +2,13 @@ import sys
 import csv
 
 def main():
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 2 or sys.argv[1] == '-h' or sys.argv[1] == '--help':
         print "Please pass me a csv file to parse!"
         return
-    
+    with open(sys.argv[1], 'rb') as bondinfo:
+        reader = csv.reader(bondinfo, delimiter=',')
+        for row in reader:
+            print row
+
+main()
 
