9f461f39c8ca14750613f36038c4c581613f74bb
Rio Kivell <rjckivel@gmail.com>
2017-10-02 23:09:39 -0400

    Finished curve and interpolation calculation.  Moving to add tests.

--- curve.py
+++ curve.py
@@ -5,10 +5,29 @@ def main():
     if len(sys.argv) < 2 or sys.argv[1] == '-h' or sys.argv[1] == '--help':
         print "Please pass me a csv file to parse!"
         return
+    # These two will be sorted by term length
     corporate_bonds, government_bonds = populateLists(sys.argv[1])
-    print corporate_bonds
-    print government_bonds
-
+    # Print header
+    print "bond,spread_to_curve"
+    # Print corporate bonds
+    for cbond in corporate_bonds:
+        # Find the two government bonds to use as 
+        # interpolation points
+        i = 0
+        while i < len(government_bonds) - 1:
+            if government_bonds[i]["term"] < cbond["term"]:
+                i = i + 1
+            else:
+                break
+        x  = cbond["term"]
+        x1 = government_bonds[i-1]["term"]
+        y1 = government_bonds[i-1]["bondYield"]
+        x2 = government_bonds[i]["term"]
+        y2 = government_bonds[i]["bondYield"]
+        m  = ((y2 - y1)/(x2 - x1))
+        y0 = ((x - x1) * m)
+        y  = round (cbond["bondYield"] - y0  - y1, 2)
+        print '%s,%s' % (cbond["name"], str(y) + '%')
 
 # Populate the government and corporate bond lists
 def populateLists(filename):
@@ -38,7 +57,7 @@ def populateLists(filename):
                         break
                 corporate_bonds.insert(i, {
                     "name": row[0],
-                    "term": round(float(row[2].split(' ')[0]), 2),
+                    "term": float(row[2].split(' ')[0]),
                     "bondYield" : float(row[3][:-1])
                 })
             elif row[1] == 'government':
@@ -52,7 +71,7 @@ def populateLists(filename):
                         break
                 government_bonds.insert(i, {
                     "name": row[0],
-                    "term": round(float(row[2].split(' ')[0]), 2),
+                    "term": float(row[2].split(' ')[0]),
                     "bondYield" : float(row[3][:-1])
                 })
             else:
